8e43cc3a37d293df1df70621e66fdf4ff2cc0640
Sam Xie <samxie@outlook.com.au>
2017-12-17 13:12:03 +1100

    Fixed browser inconsistencies

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,6 +3,29 @@ import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NotFound from './NotFoundPage';
 
+/**
+ *  **********************************************
+ *      -    THIS IS A FUTURE FEATURE    -
+ *      First we need to implement backend 
+ *      that retrieves all user bookings.
+ *  **********************************************
+ * 
+ *  UX Flow:
+ *      Basic:
+ *          -> Choose a Time and date
+ *          -> Choose a Library
+ *          -> Displays available computers and their available time to book
+ *          -? No available computers -> Advanced
+ *      Advanced:
+ *          -> Look for available timeslots for chosen library
+ *          * If not implemented, redirect to bookit.unimelb.edu.au
+ *      Finish:
+ *          * Redirects to Browse page and shows where the booking is located.
+ *          * User will get email through BookIT (automatic - unrelated)
+ *  Notes:
+ *      * For elegance, can use Time and Date picker from Material UI React Library
+ */
+
 const Book = (props) => (
     <div>
         {
